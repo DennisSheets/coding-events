9496de5479f6e29d0832fe4b4abae3e10ff710c6
madsur <dennissheets@outlook.com>
2021-05-21 11:51:11 -0500

    broken from trying to complete the bonus.  was in the process of adding an array of hashmaps, but could not figure out how to create an auto list of them.

--- coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -7,16 +7,35 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Controller
 @RequestMapping ("events")
 public class EventController {
+    public static int id = 0;
+    public static int nextID = 3;
 
     //private static ArrayList<String> events =  new ArrayList<>(Arrays.asList("geekChat", "Coder Monkeys", "Programming Church"));
-    private static List<String> events = new ArrayList<>();
+
+    private static Map<String,String> event0 = Map.of(
+            "Event","Geek Party",
+            "Description","It's a celebration of pocket protectors",
+            "Image", "geek image",
+            "Url", "geek url");
+
+    private static Map<String,String> event1 = Map.of(
+            "Event","Fun with Code",
+            "Description","Entertaining ways of using JavaScript",
+            "Image", "nerd image",
+            "Url", "nerd url");
+
+    private static Map<String,String> event2 = Map.of(
+            "Event","Typing Fanatics",
+            "Description","Not really coding, just beating the hell out our keyboards",
+            "Image", "typing image",
+            "Url", "typing url");
+
+    private static List<Map> events = new ArrayList<>(Arrays.asList(event0, event1, event2));
 
 
     @GetMapping
@@ -31,8 +50,14 @@ public class EventController {
     }
 
     @PostMapping ("create")  // lives at events/create POST
-    public String createEvent(@RequestParam String eventName){
-        events.add(eventName);
+    public String createEvent(@RequestParam String eventName, String eventDescription, String eventImageUrl, String eventUrl){
+
+        HashMap<String,String> nextID = new HashMap<>();
+            nextID.put("Event",eventName);
+            nextID.put("Description",eventDescription);
+            nextID.put("Image", eventImageUrl);
+            nextID.put("Url", eventUrl);
+        //nextID++;
         return "redirect: ";
     }
 }
