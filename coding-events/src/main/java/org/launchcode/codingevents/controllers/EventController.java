e1d2d060efec4be3a6e2c76e01410e92641a3274
madsur <dennissheets@outlook.com>
2021-06-06 21:36:37 -0500

    completed 17.3 updated to use EventRepository instead of EventData Hashmap

--- coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,8 +1,9 @@
 package org.launchcode.codingevents.controllers;
 
-import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.data.EventRepository;
 import org.launchcode.codingevents.models.Event;
 import org.launchcode.codingevents.models.EventType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -15,11 +16,16 @@ import javax.validation.Valid;
 @RequestMapping ("events")
 public class EventController {
 
+    // findAll, save, findById
+
+    @Autowired
+    private EventRepository eventRepository;
 
     @GetMapping
     public String displayAllEvents(Model model){
         model.addAttribute("title","All Events");
-        model.addAttribute("events", EventData.getAll());
+        //model.addAttribute("events", EventData.getAll());
+        model.addAttribute("events", eventRepository.findAll());
         return "events/index";
     }
 
@@ -39,7 +45,8 @@ public class EventController {
             model.addAttribute("title", "Create Events");
             return "events/create";
         }
-        EventData.add(newEvent);
+        //EventData.add(newEvent);
+        eventRepository.save(newEvent);
         return "redirect: ";
     }
 
@@ -47,7 +54,8 @@ public class EventController {
     @GetMapping ("delete")
     public String displayDeleteEventForm(Model model){
         model.addAttribute("title", "Delete Events");
-        model.addAttribute("events",EventData.getAll());
+       //model.addAttribute("events",EventData.getAll());
+        model.addAttribute("events",eventRepository.findAll());
         return "events/delete";
     }
 
@@ -55,7 +63,8 @@ public class EventController {
     public String processEventDelete(@RequestParam (required = false) int[] eventIds) {
         if(eventIds != null) {
             for (int id : eventIds) {
-                EventData.remove(id);
+                //EventData.remove(id);
+                eventRepository.deleteById(id);
             }
         }
         return "redirect:";
